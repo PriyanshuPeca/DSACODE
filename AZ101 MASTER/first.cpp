be52784a5f4b2ea16c183f62af460fe0d9f6c560
PriyanshuPeca <priyanshu22223@gmail.com>
2026-06-20 18:15:51 +0530

    changes

--- AZ101 MASTER/first.cpp
+++ AZ101 MASTER/first.cpp
@@ -1,7 +1,17 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    cout<<"helloc";
-    return 0;
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i]<<" ";
+    }
 }
