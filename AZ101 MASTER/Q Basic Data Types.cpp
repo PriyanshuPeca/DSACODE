d62fef382ffe7f61b650d6e9826c117c40e9d8aa
Priyanshu <priyanshu22223@gmail.com>
2026-06-22 17:57:07 +0530

    bsaic data types

---
+++ AZ101 MASTER/Q Basic Data Types.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+const int MOD = 1e9+7;
+const int INF= LLONG_MAX>>1;
+
+void solve(){
+    //code here
+    int i,l;
+    char c;
+    double f,d;
+    cin>>i>>l>>c>>f>>d;
+    cout<<i<<endl;
+    cout<<l<<endl;
+    cout<<c<<endl;
+    cout<<f<<endl;
+    cout<<d<<endl;
+}
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);    //cout.tie(NULL);
+    int t;
+    t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
